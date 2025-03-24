26878edf2400b8021f18d77d55da58814caa9be9
lhgenol <lhgenol@gmail.com>
2025-03-24 09:49:43 +0900

    STEP 10: try

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,7 +22,8 @@ public class GameManager : MonoBehaviour
             return;
         }
         
-        userData = new UserData("이유림", 100000, 50000);
+        // ResetUserData();
+        LoadUserData();
     }
 
     public void Start()
@@ -32,26 +33,83 @@ public class GameManager : MonoBehaviour
             userInfo = FindObjectOfType<UserInfo>();
         }
         
-        UpdateName("이현");
-        UpdateCash(0);
-        UpdateBalance(0);
+        // UpdateName("OOO");
+        // UpdateCash(0);
+        // UpdateBalance(0);
+        
+        SaveUserData();
     }
     
     public void UpdateName(string newName)
     {
         userData.name = newName;
         userInfo.Refresh();
+        
+        SaveUserData();
     }
     
     public void UpdateCash(int amount)
     {
         userData.cash += amount;
         userInfo.Refresh();
+        
+        SaveUserData();
     }
 
     public void UpdateBalance(int amount)
     {
         userData.balance += amount;
         userInfo.Refresh();
+        
+        SaveUserData();
+    }
+
+    public void SaveUserData()  // 데이터 저장
+    {
+        PlayerPrefs.SetString("Name", userData.name);
+        PlayerPrefs.SetInt("Cash", userData.cash);
+        PlayerPrefs.SetInt("Balance", userData.balance);
+        PlayerPrefs.Save();
+        
+        Debug.Log("Saved");
+    }
+
+    public void LoadUserData()  // 데이터 로드
+    {
+        if (PlayerPrefs.HasKey("Name"))
+        {
+            string name = PlayerPrefs.GetString("Name");
+            int cash = PlayerPrefs.GetInt("Cash");
+            int balance = PlayerPrefs.GetInt("Balance");
+            userData = new UserData(name, cash, balance);
+        }
+        else
+        {
+            userData = new UserData("이현", 100001, 50001);
+        }
+        
+        Debug.Log("Loaded");
+    }
+
+    public void ResetUserData() // 데이터 삭제
+    {
+        PlayerPrefs.DeleteKey("Name");
+        PlayerPrefs.DeleteKey("Cash");
+        PlayerPrefs.DeleteKey("Balance");
+        PlayerPrefs.Save();
+        
+        userData = new UserData("이현", 100000, 50000);
+        
+        if (userInfo != null)
+        {
+            userInfo.Refresh();
+        }
+        
+        Debug.Log("Reseted");
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveUserData();
     }
 }
